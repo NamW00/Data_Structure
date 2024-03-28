6c5712e56d86a3b7512ca5b98a2586830028b943
NamWoo <guddn9477@daum.net>
2024-03-28 21:48:02 +0900

    (2024.3.28) 해시 테이블

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -43,18 +43,139 @@ public:
 
 	// template 특수화 : 예외 처리
 	// HashFunction template 특수화
-	//	template <typename T>
-	//	int HashFunction(T key)
-	//	{
-	//		int sum = 0;
-	//		string hashIndex[SIZE];
-	//		for (int i = 0; i <; i++)
-	//		{
-	//			
-	//		}
-	//		return hashIndex;
-	//	}
+	template <>
+	int HashFunction(string key)
+	{
+		int result = 0;
 
+		for (const char & element : key )
+		{
+			result += (int)element;
+		}
+
+		//	for (int i = 0; i < key.length(); i++)
+		//	{
+		//		result += (int)key[i];
+		//	}
+
+		int hashIndex = result % SIZE;
+
+		return hashIndex;
+	}
+
+	Node* CreateNode(KEY key, VALUE value)
+	{
+		Node* newNode = new Node;
+
+		newNode->key = key;
+
+		newNode->value = value;
+
+		newNode->next = nullptr;
+
+		// 생성한 노드의 주소값
+		return newNode;
+	}
+
+	void Insert(KEY key, VALUE value)
+	{
+		// 1. 해쉬 함수를 통해서 값을 받는 임시 변수
+		int tmp = HashFunction(key);
+		// 2. 새로운 노드를 생성합니다.
+		Node* newNode = CreateNode(key, value);
+
+		if (bucket[tmp].count == 0)
+		{
+			// 1. bucket[tmp]의 head 포인터에 새로운 노드를 저장합니다.
+			// -> 노드가 1개라도 존재하지 않는다면 헤드는 새로운 노드의 시작주소를 가리키고 있어야한다.
+			bucket[tmp].head = newNode;
+
+			// 2. bucket[tmp]의 count 변수의 값을 증가 시킵니다.
+			bucket[tmp].count++;
+		}
+		else
+		{
+			// 1. newNode의 next에 bucket[tmp]의 head값을 저장합니다.
+			newNode->next = bucket[tmp].head;
+
+			// 2. bucket[tmp].head를 방금 새로 생성한 노드의 주소를 가리키게 합니다.
+			bucket[tmp].head = newNode;
+
+			// 3. bucket[tmp]의 count 변수의 값을 증가시킵니다.
+			bucket[tmp].count++;
+		}
+	}
+
+	// bucket[]은 정적할당을 했기 때문에 '->' 가 아니라 '.'으로 내부의 데이터로 접근을 해야한다.
+	// Node와 같은 동적할당을 사용한 구조체일 경우에는 '->'
+	void Show()
+	{
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* showNode = bucket[i].head;
+
+			while (showNode != nullptr)
+			{
+				cout << "bucket[" << i << "]" << endl;
+				cout << "Key 값 : " << showNode->key << endl;
+				cout << "Value 값 : " << showNode->value << endl;
+				showNode = showNode->next;
+			}
+			cout << endl;
+
+		}
+	}
+
+	void Remove(KEY key)
+	{
+		int hashIndex = HashFunction(key);
+		Node* deleteNode = bucket[hashIndex].head;
+		Node* traceNode = deleteNode;
+
+		
+		if (deleteNode == nullptr)
+		{
+			cout << "bucket Is Empty~!" << endl;
+		}
+		else
+		{
+			// 2. 내가 삭제하고 싶은 key값과 head가 가리키는 key값을 비교한다.
+			// -> deleteNode = deleteNode->next
+			while (deleteNode->key != key)
+			{
+				traceNode = deleteNode;
+				deleteNode = traceNode->next;
+				traceNode->next = deleteNode;
+			}
+
+			
+		}
+
+		if (deleteNode == nullptr)
+		{
+			traceNode->next = nullptr;
+			deleteNode = nullptr;
+			delete deleteNode;
+		}
+		else
+		{
+			traceNode->next = deleteNode->next;
+			deleteNode->next = nullptr;
+			delete deleteNode;
+		}
+		
+		
+		
+
+		
+		
+		// 3. 이전노드의 주소값을 저장할수 있는 node : traceNode
+		// -> deleteNode 이전 노드를 가리키고 있어야한다.
+
+
+
+	}
 	
 	
 	// 동적 할당을 하므로 소멸자도 해주어야 한다.
@@ -94,6 +215,18 @@ int main()
 
 
 #pragma endregion
+	HashTable<int, string> hashTable;
+
+	hashTable.Insert(10, "Soccer");
+	hashTable.Insert(27, "BaseBall");
+	hashTable.Insert(14, "Tennis");
+	hashTable.Insert(97, "BasketBall");
+	hashTable.Insert(99, "Swimming");
+
+	hashTable.Show();
+
+	hashTable.Remove(99);
+	hashTable.Show();
 
 
 	return 0;
